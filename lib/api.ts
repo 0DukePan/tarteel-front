da54886e41f5fa6f8e09c5460d4a1160d15b0272
0DukePan <allforoneafo7@gmail.com>
2025-07-18 02:20:27 +0100

    updates

--- lib/api.ts
+++ lib/api.ts
@@ -18,7 +18,7 @@ import type {
   ApiResponse,
 } from './types';
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'https://tarteel-back.onrender.com/api'
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://tarteel-back.onrender.com';
 
 console.log(API_BASE_URL);
 
