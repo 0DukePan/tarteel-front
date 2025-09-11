72f050ae7e8bab511c3917dd59a161f5e0e274f8
0DukePan <allforoneafo7@gmail.com>
2025-09-11 18:49:15 +0100

    up

--- app/admin/dashboard/page.tsx
+++ app/admin/dashboard/page.tsx
@@ -1,12 +1,5 @@
-"use client";
-
 import { AdminDashboard } from "@/app/components/admin/admin-dashboard";
-import { AdminLayout } from "@/app/components/admin/admin-layout";
 
 export default function DashboardPage() {
-  return (
-    <AdminLayout>
-      <AdminDashboard />
-    </AdminLayout>
-  );
+  return <AdminDashboard />;
 }
