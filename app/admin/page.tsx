4a96e151b52ea82aa7c9c57bb51f9fa34c7ee65b
0DukePan <allforoneafo7@gmail.com>
2025-09-11 18:11:55 +0100

    up

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -3,6 +3,9 @@ import { AdminDashboard } from "../components/admin/admin-dashboard";
 import { redirect } from "next/navigation";
 
 export default async function AdminPage() {
+  console.log("🔎 API_URL:", process.env.API_URL);
+  console.log("🔎 NEXT_PUBLIC_API_URL:", process.env.NEXT_PUBLIC_API_URL);
+
   const token = await getAuthTokenServer();
   if (!token) {
     redirect('/admin/login'); 
